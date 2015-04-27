1de3bd9625ae89421aba52bfc251266990ab034a
Chelsea Okey <chelsea.okey@gmail.com>
2015-04-27 17:12:31 -0500

    finished product

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-var apikey = 'YOUR-API-KEY'; // Put your API key here
 
-// Use this function to do stuff with your results.
-// It is called after 'search' is executed.
 function searchCallback(results) {
 	for (var i = 0; i < 10; i++) {
 		var platformName = "";
 		for (var j = 0; j < results[i].platforms.length; j++) {
-			platformName += results[i].platforms[j].name + ", ";
+			platformName += " - " + results[i].platforms[j].name;
 		}
-		$('#searchResults').append(
-			'<div id="result' + (i+1) + '"' +
-				'<div class="row" id="name">' + results[i].name + '</div>' +
-				'<div class="row" id="image"><img src="' + results[i].image.thumb_url + '"/></div>' +
-				'<div class="row" id="description">' + results[i].deck + '</div>' +
-				'<div class="row" id="platforms">' + platformName + '</div>' +
-			'</div>'
-		);
-		console.log("Nothing: " + results);
+		if((i+1) % 3 == 0) {
+			$('#searchResults').append(
+				'<div id="searchRow" class="row">' +
+					'<div id="result' + (i+1) + '" class="col-md-4">' +
+						'<div id="name"><h4>Game Title:</h4> ' + results[i].name + '</div>' +
+						'<div id="image"><img src="' + results[i].image.thumb_url + '"/></div>' +
+						'<div id="description"><h5>Description:</h5> ' + results[i].deck + '</div>' +
+						'<div id="platforms"><h5>Supported Platforms:</h5> ' + platformName + '</div>' +
+					'<div>' +
+				'</div>'
+			);
+		} else {
+			$('#searchResults').append(
+				'<div id="result' + (i+1) + '" class="col-md-4">' +
+					'<div id="name"><h4>Game Title:</h4> ' + results[i].name + '</div>' +
+					'<div id="image"><img src="' + results[i].image.thumb_url + '"/></div>' +
+					'<div id="description"><h5>Description:</h5> ' + results[i].deck + '</div>' +
+					'<div id="platforms"><h5>Supported Platforms:</h5> ' + platformName + '</div>' +
+				'<div>'
+			);
+		}
 	}
 }
 
@@ -29,7 +38,6 @@ $(document).ready(function() {
 		userInput = $('#search').val();
 		search(userInput);
 	});
-	// Start the search here!
 });
 
 // HELPER FUNCTION
